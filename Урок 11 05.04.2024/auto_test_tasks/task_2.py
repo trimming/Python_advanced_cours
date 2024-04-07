b1223e501904af5a8b406908f3f647c27d2038b5
Alexey Golikov <lexgolikoff@gmail.com>
2024-04-07 23:21:40 +0300

    some update

---
+++ Урок 11 05.04.2024/auto_test_tasks/task_2.py
@@ -0,0 +1,26 @@
+class Archive:
+    archive_number = None
+    archive_text = None
+    _instance = None    
+
+    def __new__(cls, archive_number, archive_text, *args):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            archive_text = []
+            archive_number = []
+        cls.archive_text.append(args[0])
+        cls.archive_number.append(args[1])
+        return cls._instance
+
+    def __init__(self, text: str, number: int | float):
+        self.text = text
+        self.number = number
+
+
+if __name__ == '__main__':
+    rec_1 = Archive('Record 1', 45)
+    rec_2 = Archive('Record 2', 45.34)
+    rec_3 = Archive('Record 3', 5.05)
+    print(rec_1.archive_text, rec_1.archive_number)
+    print(rec_2.archive_text, rec_2.archive_number)
+    print(rec_3.archive_text, rec_3.archive_number)
--- Урок 11 05.04.2024/task_2.py
+++ Урок 11 05.04.2024/task_2.py
@@ -7,25 +7,25 @@
 class Archive:
     arch = []
 
-    # def __new__(cls, *args):
-    #     instance = super().__new__(cls)
-    #     instance.arch = cls.arch.copy()
-    #     cls.arch.append(instance)
-    #     return instance
+    def __new__(cls, *args):
+        instance = super().__new__(cls)
+        instance.arch = cls.arch.copy()
+        cls.arch.append(instance)
+        return instance
 
     def __init__(self, number, letter):
         Archive.arch.append([number, letter])
         self.number = number
         self.letter = letter
 
+    # def __repr__(self):
+    #     return f'{self.number} {self.letter}'
+
+    def __str__(self):
+        return f'STR: {self.number} {self.letter}'
+
     def __repr__(self):
-        return f'{self.number} {self.letter}'
-
-        def __str__(self):
-            return f'STR: {self.number} {self.letter}'
-
-        def __repr__(self):
-            return f'REPR: {self.number} {self.letter}'
+        return f'REPR: {self.number} {self.letter}'
 
 
 a = Archive(1, 'a')
@@ -34,10 +34,10 @@ b = Archive(2, 'b')
 print(a.__repr__())
 print(b)
 c = [a, b]
-print(str(c))
+# print(str(c))
 
-a = Archive(1, 'a')
-b = Archive(2, 'b')
+# a = Archive(1, 'a')
+# b = Archive(2, 'b')
 # c = Archive(3, 'c')
 # d = Archive(4, 'd')
 
