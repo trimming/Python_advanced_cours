5006946c99b10d1612975e9e42b23723d5e394f7
Alexey Golikov <lexgolikoff@gmail.com>
2024-04-13 11:57:45 +0300

    some update

--- Урок 11 05.04.2024/auto_test_tasks/task_4.py
+++ Урок 11 05.04.2024/auto_test_tasks/task_4.py
@@ -1,15 +1,17 @@
 class Matrix:
     count = 0
+
     def __init__(self, rows: int, cols: int):
         self.rows = rows
         self.cols = cols
+        # self.data = [[0 for _ in range(self.cols)] for _ in range(rows)]
         self.data = self.create_data()
 
     def create_data(self):
         data = []
         for i in range(self.rows):
             in_data = []
-            for _ in range(self.cols):                
+            for _ in range(self.cols):
                 Matrix.count += 1
                 in_data.append(Matrix.count)
             data.append(in_data)
@@ -21,7 +23,7 @@ class Matrix:
             row = [str(i) for i in row]
             line += ' '.join(row) + '\n'
         return line
-    
+
     def __repr__(self):
         return f'Matrix({self.rows}, {self.cols})'
 
@@ -33,19 +35,24 @@ class Matrix:
                 return False
         else:
             raise TypeError('Ошибка класса!')
-        
-   def __add__(self, other):
-       if isinstance(other, Matrix):
-           
+
+    def __add__(self, other):
+        if isinstance(other, Matrix):
             if self.rows == other.rows and self.cols == other.cols:
-                for i in range(self.rows):
-                    for j in range(self.cols):
-                                
-                
+                return [[self.data[i][j] + other.data[i][j] for j in range(self.cols)] for i in range(self.rows)]
         else:
             raise TypeError('Ошибка класса!')
 
-mx = Matrix(4, 5)
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            if self.rows == other.cols and self.cols == other.rows:
+
+        else:
+            raise TypeError('Ошибка класса!')
+
+
+mx = Matrix(3, 2)
 mx2 = Matrix(3, 2)
 print(repr(mx))
-print(mx == mx2)
+print(mx + mx2)
+print(mx.data)
