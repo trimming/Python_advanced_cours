d55b82c90d8b4785eba1393b993a6fc749415ed3
Alexey Golikov <lexgolikoff@gmail.com>
2024-04-16 00:24:00 +0300

    added task_1 from seminar_12

--- Урок 12 10.04.2024/auto_test_task/task_1.py
+++ Урок 12 10.04.2024/auto_test_task/task_1.py
@@ -1,27 +1,43 @@
-
 import csv
 from typing import Any
 
 
 class Student:
-    # name = Validate()
-    # subjects = Validate()
+    """
+        Класс, представляющий студента.
+
+        Атрибуты:
+        - name (str): ФИО студента
+        - subjects (dict): словарь, содержащий предметы и их оценки и результаты тестов
+
+        Методы:
+        - __init__(self, name, subjects_file): конструктор класса
+        - __setattr__(self, name, value): дескриптор, проверяющий ФИО на первую заглавную букву и наличие только букв
+        - __getattr__(self, name): получение значения атрибута
+        - __str__(self): возвращает строковое представление студента
+        - load_subjects(self, subjects_file): загрузка предметов из файла CSV
+        - get_average_test_score(self, subject): возвращает средний балл по тестам для заданного предмета
+        - get_average_grade(self): возвращает средний балл по всем предметам
+        - add_grade(self, subject, grade): добавление оценки по предмету
+        - add_test_score(self, subject, test_score): добавление результата теста по предмету
+        """
     def __init__(self, name: str, subjects_file):
         self.name = name
-        self.subjects = self.load_subjects(subjects_file)
+        self._load_subjects = self.load_subjects(subjects_file)
+        self.subjects = {}
 
     def __setattr__(self, name, value):
         if name == self.name:
             val = True
             for n in value.split():
-                if n.isalpha() and n.istitle(): 
+                if n.isalpha() and n.istitle():
                     continue
                 else:
                     val = False
-            if val:        
+            if val:
                 self.__dict__[name] = value
             else:
-                print('ФИО должно состоять только из букв и начинаться с заглавной буквы')
+                raise ValueError('ФИО должно состоять только из букв и начинаться с заглавной буквы')
         else:
             self.__dict__[name] = value
 
@@ -29,48 +45,60 @@ class Student:
         return item
 
     def __str__(self):
-        return f'Студент: {self.name}\nПредметы:'
+        return f'Студент: {self.name}\nПредметы: {", ".join([key for key in self.subjects.keys()])}'
 
     def load_subjects(self, file: Any):
         with open(file, 'r', newline='', encoding='utf-8') as f:
             csv_file = csv.reader(f)
-            data = {}
             for line in csv_file:
-                data[line[0]] = [[], []]
-            return data
-        
+                return line
+
     def add_grade(self, subject, grade):
-        if subject in self.subjects:
+        if subject in self._load_subjects:
             if type(grade) is int and 2 <= grade <= 5:
-                self.subjects[subject][0].append(grade)
+                if subject in self.subjects:
+                    self.subjects[subject]['grade'].append(grade)
+                else:
+                    self.subjects[subject] = {'grade': [grade], 'test_score': []}
             else:
-                print('Оценка должна быть целым числом от 2 до 5')
+                raise ValueError('Оценка должна быть целым числом от 2 до 5')
         else:
-            print(f'Предмет {subject} не найден')
+            raise ValueError(f'Предмет {subject} не найден')
 
     def add_test_score(self, subject, test_score):
         if subject in self.subjects:
             if type(test_score) is int and 0 <= test_score <= 100:
-                self.subjects[subject][1].append(test_score)
+                if subject in self.subjects:
+                    self.subjects[subject]['test_score'].append(test_score)
+                else:
+                    self.subjects[subject] = {'test_score': [test_score], 'grade': []}
             else:
-                print('Результат теста должен быть целым числом от 0 до 100')
+                raise ValueError('Результат теста должен быть целым числом от 0 до 100')
         else:
-            print(f'Предмет {subject} не найден')
+            raise ValueError(f'Предмет {subject} не найден')
 
     def get_average_test_score(self, subject):
-        return sum(self.subjects[subject][1]) / len(self.subjects[subject][1])
+        if subject in self.subjects:
+            return sum(self.subjects[subject]['test_score']) / len(self.subjects[subject]['test_score'])
+        else:
+            raise ValueError(f'Предмет {subject} не найден')
 
     def get_average_grade(self):
         av_grade = 0
         count = 0
         for value in self.subjects.values():
-            av_grade += sum(value[0])
-            count += len(value[0])
-        return av_grade / count
-            
+            print(value['grade'])
+            av_grade += sum(value['grade'])
+            count += len(value['grade'])
+        if count > 0:
+            return av_grade / count
+        else:
+            return 0
+
+
 if __name__ == '__main__':
     st1 = Student('Иван Иванович Иванов', 'subjects.csv')
-    print(st1)
+
     st1.add_grade('История', 3)
     st1.add_grade('Математика', 3)
     st1.add_grade('История', 5)
@@ -78,7 +106,9 @@ if __name__ == '__main__':
     st1.add_grade('Физика', 2)
     st1.add_test_score("Математика", 85)
     st1.add_test_score("Математика", 60)
-    avg = st1.get_average_test_score('Математика')
+    st1.add_test_score("Физика", 90)
+    avg = st1.get_average_test_score('Физика')
     avg_g = st1.get_average_grade()
     print(avg_g)
     print(st1.subjects)
+    print(st1)
