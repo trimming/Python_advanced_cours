07afcc0a68b06f7b1673b609f09669078f6f2d8d
trimming <lexgolikoff@gmail.com>
2024-04-22 16:31:54 +0300

    some update

---
+++ Урок 14 21.04.2024/auto_test_tasks/task_1.py
@@ -0,0 +1,141 @@
+##import doctest
+
+
+class NegativeValueError(ValueError):
+    def __init__(self, message: str):
+        self.message = message
+
+    def __str__(self):
+        return f'{self.message}'
+
+
+class Rectangle:
+    """
+    >>> r1=Rectangle(5)
+    >>> r1.width
+    5
+
+    """
+    
+    def __init__(self, width, height=None):
+        
+        if width > 0:
+            self.width = width            
+        else:
+            raise NegativeValueError(f'Ширина должна быть положительной, а не {width}')        
+        if height > 0:
+            if height is None:
+                self.height = width
+            else:
+                self.height = height
+        else:
+            raise NegativeValueError(f'Высота должна быть положительной, а не {height}')
+
+    def perimeter(self):
+        """
+        Вычисляет периметр прямоугольника.
+
+        Возвращает:
+        - int: периметр прямоугольника
+        """
+        return 2 * (self.width + self.height)
+
+    def area(self):
+        """
+        Вычисляет площадь прямоугольника.
+
+        Возвращает:
+        - int: площадь прямоугольника
+        """
+        return self.width * self.height
+
+    def __add__(self, other):
+        """
+        Определяет операцию сложения двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - Rectangle: новый прямоугольник, полученный путем сложения двух исходных прямоугольников
+        """
+        width = self.width + other.width
+        perimeter = self.perimeter() + other.perimeter()
+        height = perimeter / 2 - width
+        return Rectangle(width, height)
+
+    def __sub__(self, other):
+        """
+        Определяет операцию вычитания одного прямоугольника из другого.
+
+        Аргументы:
+        - other (Rectangle): вычитаемый прямоугольник
+
+        Возвращает:
+        - Rectangle: новый прямоугольник, полученный путем вычитания вычитаемого прямоугольника из исходного
+        """
+        if self.perimeter() < other.perimeter():
+            self, other = other, self
+        width = abs(self.width - other.width)
+        perimeter = self.perimeter() - other.perimeter()
+        height = perimeter / 2 - width
+        return Rectangle(width, height)
+
+    def __lt__(self, other):
+        """
+        Определяет операцию "меньше" для двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - bool: True, если площадь первого прямоугольника меньше площади второго, иначе False
+        """
+        return self.area() < other.area()
+
+    def __eq__(self, other):
+        """
+        Определяет операцию "равно" для двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - bool: True, если площади равны, иначе False
+        """
+        return self.area() == other.area()
+
+    def __le__(self, other):
+        """
+        Определяет операцию "меньше или равно" для двух прямоугольников.
+
+        Аргументы:
+        - other (Rectangle): второй прямоугольник
+
+        Возвращает:
+        - bool: True, если площадь первого прямоугольника меньше или равна площади второго, иначе False
+        """
+        return self.area() <= other.area()
+
+    def __str__(self):
+        """
+        Возвращает строковое представление прямоугольника.
+
+        Возвращает:
+        - str: строковое представление прямоугольника
+        """
+        return f"Прямоугольник со сторонами {self.width} и {self.height}"
+
+    def __repr__(self):
+        """
+        Возвращает строковое представление прямоугольника, которое может быть использовано для создания нового объекта.
+
+        Возвращает:
+        - str: строковое представление прямоугольника
+        """
+        return f"Rectangle({self.width}, {self.height})"
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=False)    
