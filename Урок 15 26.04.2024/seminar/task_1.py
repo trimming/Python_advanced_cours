3b9b23043fa53aa5355f7a3eb8727dde2e3781b2
trimming <lexgolikoff@gmail.com>
2024-04-26 16:47:15 +0300

    added dir lesson_15

---
+++ Урок 15 26.04.2024/seminar/task_1.py
@@ -0,0 +1,10 @@
+##Задание №1
+##Напишите программу, которая использует модуль logging для
+##вывода сообщения об ошибке в файл.
+##Например отлавливаем ошибку деления на ноль.
+import logging
+
+def logging_info(file_name: str):
+    logging.basicConfig(filename=file_name, filemode='w', encoding='utf-8'
+    logger = 
+    
